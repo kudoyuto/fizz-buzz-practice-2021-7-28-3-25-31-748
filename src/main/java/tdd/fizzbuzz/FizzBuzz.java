58937daffc3732913523a36e799db117edc913a4
kudoyuto <yuto.kudo@oocl.com>
2021-08-05 13:54:08 +0800

    [fizzbuzz][Yuto] feat: implementation of the multiple of 7 test case

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,9 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public static String countOff(int count){
+        if(count % 7 ==0){
+            return String.valueOf(count);
+        }
         return null;
     }
 }
